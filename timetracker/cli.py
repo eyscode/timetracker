31ab67a16b79f788a750568212d7ecea9c3780d8
Oscar Arbeláez <odarbelaeze@gmail.com>
2020-12-26 13:09:37 -0500

    Revert "Relax requirements update csv stuff to support stdin"
    This reverts commit aa0ff874483b5df59669b0e49aa658b309e33f39.

--- timetracker/cli.py
+++ timetracker/cli.py
@@ -14,30 +14,36 @@ def tt():
 
 @tt.command()
 @click.option(
-    "--text",
-    "-t",
-    help="What did you do?",
+    '--text', '-t',
+    help='What did you do?',
 )
 @click.option(
-    "--date", "-d", help="When did you do it?", default="today", callback=validate_date
+    '--date', '-d',
+    help='When did you do it?',
+    default='today',
+    callback=validate_date
 )
 @click.option(
-    "--config",
-    "-c",
+    '--config', '-c',
     default=CONFIG_PATH,
     type=click.Path(exists=True, dir_okay=False),
-    help="Path to a config file",
+    help='Path to a config file'
 )
 @click.option(
-    "--pto", "-p", default=False, is_flag=True, help="Is this day paid time off"
+    '--pto', '-p',
+    default=False,
+    is_flag=True,
+    help='Is this day paid time off'
 )
 @click.option(
-    "--vacations", "-v", default=False, is_flag=True, help="Is this day paid time off"
+    '--vacations', '-v',
+    default=False,
+    is_flag=True,
+    help='Is this day paid time off'
 )
 @click.option(
-    "--hours",
-    "-h",
-    help="How long did it take?",
+    '--hours', '-h',
+    help='How long did it take?',
 )
 def load(text, config, date, pto, vacations, hours):
     """
@@ -48,23 +54,40 @@ def load(text, config, date, pto, vacations, hours):
 
 @tt.command()
 @click.option(
-    "--config",
-    "-c",
+    '--config', '-c',
     default=CONFIG_PATH,
     type=click.Path(exists=True, dir_okay=False),
-    help="Path to a config file",
+    help='Path to a config file'
 )
-@click.option("--date", "-d", help="Date", callback=validate_date)
 @click.option(
-    "--start",
-    "-s",
-    help="Starting date",
-    default=begin_of_month,
-    callback=validate_date,
+    '--date', '-d',
+    help='Date',
+    callback=validate_date
 )
-@click.option("--end", "-e", help="End date", default="today", callback=validate_date)
-@click.option("--full", "-f", is_flag=True, help="Show full table", default=False)
-@click.option("--weekday", "-w", is_flag=True, help="Show week day", default=False)
+@click.option(
+    '--start', '-s',
+    help='Starting date',
+    default=begin_of_month,
+    callback=validate_date
+)
+@click.option(
+    '--end', '-e',
+    help='End date',
+    default='today',
+    callback=validate_date
+)
+@click.option(
+    '--full', '-f',
+    is_flag=True,
+    help='Show full table',
+    default=False
+)
+@click.option(
+    '--weekday', '-w',
+    is_flag=True,
+    help='Show week day',
+    default=False
+)
 def show(config, date, start, end, full, weekday):
     """
     Show loaded hours
@@ -72,21 +95,21 @@ def show(config, date, start, end, full, weekday):
     start = date if date else start
     end = date if date else end
     if end and not start:
-        raise click.MissingParameter(f"End date requires a starting date")
+        raise click.MissingParameter(
+            f'End date requires a starting date')
     show_hours(config, start, end, full, weekday)
 
 
 @tt.command()
 @click.argument(
-    "csv_file",
-    type=click.File(mode="r"),
+    'csv_file',
+    type=click.Path(exists=True, dir_okay=False),
 )
 @click.option(
-    "--config",
-    "-c",
+    '--config', '-c',
     default=CONFIG_PATH,
     type=click.Path(exists=True, dir_okay=False),
-    help="Path to a config file",
+    help='Path to a config file'
 )
 def load_csv(csv_file, config):
     """
